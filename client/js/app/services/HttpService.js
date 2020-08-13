2d464045471444ec0e09bbe75e68af596df28a4f
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2020-08-13 16:00:45 -0300

    Simplificando requisições Ajax com Fetch API

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,23 +1,27 @@
 class HttpService {
 
+    _handleErrors(res) {
+        if (res.ok) {
+            return res;
+        } else {
+            throw new Error(res.statusText);
+        }
+    }
+
     get(url) {
 
-        return new Promise((resolve, reject) => {
+        return fetch(url)
+            .then(res => this._handleErrors(res))
+            .then(res => res.json());
+    }
 
-            let xhr = new XMLHttpRequest();
-            xhr.open('GET', url);
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        console.log(xhr.responseText);
-                        reject(xhr.responseText);
-                    }
-                }
-            }
+    post(url, dado) {
 
-            xhr.send();
-        });
+        return fetch(url, {
+                headers: { 'Content-Type': 'application/json' },
+                method: 'post',
+                body: JSON.stringify(dado)
+            })
+            .then(res => this._handleErrors(res));
     }
 }
